033cfbf57e0703d60e8a6b5696c63f96b29c9148
Ryan Caldwell <ryanjcaldwell4@gmail.com>
2021-08-29 22:06:55 -0400

    Initial commit

---
+++ lib/chess_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "chess_api/version"
+
+module ChessApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/chess_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ChessApi do
+  it "has a version number" do
+    expect(ChessApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
